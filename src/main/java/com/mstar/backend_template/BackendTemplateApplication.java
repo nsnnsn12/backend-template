dd23c574583842c77ea5e934d428bfd4b7f89b54
Noh Sung Gyu <gkdlshtjdrb@naver.com>
2025-04-03 16:53:07 +0900

    first commit

---
+++ src/main/java/com/mstar/backend_template/BackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mstar.backend_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTemplateApplication.class, args);
+	}
+
+}
